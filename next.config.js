9bc6e3fc999fa41871c265fbee336ade25248cfc
SarmientoCreator <sarmientocreator@gmail.com>
2025-03-24 21:58:06 +0100

    fix: simplify CSP configuration

--- next.config.js
+++ next.config.js
@@ -6,14 +6,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: [
-              "default-src 'self'",
-              "font-src 'self' data: 'unsafe-inline'",
-              "style-src 'self' 'unsafe-inline'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
-              "img-src 'self' data: blob:",
-              "connect-src 'self'",
-            ].join('; ')
+            value: "default-src 'self'; font-src 'self' data: 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
           }
         ]
       }
