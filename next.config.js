e7a6346c068055c9a5d80360024d319011632d74
SarmientoCreator <sarmientocreator@gmail.com>
2025-03-24 21:41:49 +0100

    fix: update CSP configuration with comprehensive rules

--- next.config.js
+++ next.config.js
@@ -6,11 +6,14 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              font-src 'self' data: 'unsafe-inline';
-              style-src 'self' 'unsafe-inline';
-            `
+            value: [
+              "default-src 'self'",
+              "font-src 'self' data: 'unsafe-inline'",
+              "style-src 'self' 'unsafe-inline'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+              "img-src 'self' data: blob:",
+              "connect-src 'self'",
+            ].join('; ')
           }
         ]
       }
